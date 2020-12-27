4c5421908ab01a04ed156461c38000b772126030
Daniel Leong <me@dhleong.net>
2020-12-26 22:43:52 -0500

    Support providing config to the Device factories; add withAddress

--- src/device.ts
+++ src/device.ts
@@ -1,4 +1,7 @@
 import { PendingDevice } from "./device/pending";
+import { IDiscoveryConfig, INetworkConfig } from "./discovery/model";
+
+export type Config = INetworkConfig & Partial<IDiscoveryConfig>;
 
 /**
  * IDevice factories
@@ -7,15 +10,43 @@ export const Device = {
     /**
      * Return a Device that talks to the first one found on the network
      */
-    any() {
-        return new PendingDevice("any", () => true);
+    any(config: Config = {}) {
+        return new PendingDevice(
+            "any",
+            () => true,
+            config,
+            config,
+        );
+    },
+
+    /**
+     * Create a Device that talks to the device with the given address
+     */
+    withAddress(
+        address: string,
+        config: Config = {},
+    ) {
+        return new PendingDevice(
+            `with address ${address}`,
+            device => device.address === address,
+            config,
+            config,
+        );
     },
 
     /**
      * Create a Device that talks to the first device on the network
      * with the given ID
      */
-    withId(id: string) {
-        return new PendingDevice(`with id ${id}`, device => device.id === id);
+    withId(
+        id: string,
+        config: Config = {},
+    ) {
+        return new PendingDevice(
+            `with id ${id}`,
+            device => device.id === id,
+            config,
+            config,
+        );
     },
 };
