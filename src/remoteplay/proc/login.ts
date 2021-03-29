2fb3c79e7e735f7b4848274d788f7781a1c6d8b7
Daniel Leong <me@dhleong.net>
2021-03-29 09:45:44 -0400

    Add logging to LoginProc

--- src/remoteplay/proc/login.ts
+++ src/remoteplay/proc/login.ts
@@ -1,3 +1,5 @@
+import _debug from "debug";
+
 import { IConnectionConfig } from "../../device/model";
 import { performRpc, RpcError } from "../../socket/helpers";
 import { IDeviceProc, IDeviceSocket } from "../../socket/model";
@@ -9,21 +11,27 @@ import { RemotePlayPasscodeResponsePacket } from "../packets/passcode-response";
 
 type Result = RemotePlayLoginResultPacket | RemotePlayPasscodeRequestPacket;
 
+const debug = _debug("playactor:remoteplay:LoginProc");
+
 export class RemotePlayLoginProc implements IDeviceProc {
     constructor(
         public config: IConnectionConfig,
     ) {}
 
     public async perform(socket: IDeviceSocket) {
+        debug("Performing Login");
         const response = await performRpc<Result>(
             socket,
             new RemotePlayOutgoingPacket(RemotePlayCommand.Login),
             RemotePlayResponseType.Login, RemotePlayResponseType.Passcode,
         );
 
+        debug("Received: ", response);
         if (response.type === RemotePlayResponseType.Passcode) {
+            debug("Passcode was required");
             const passCode = this.config.login?.passCode;
             if (!passCode || !passCode.length) {
+                debug("... but no passcode provided", this.config.login);
                 throw new RpcError(
                     1,
                     LoginResultError.PASSCODE_IS_NEEDED,
@@ -31,11 +39,14 @@ export class RemotePlayLoginProc implements IDeviceProc {
             }
 
             // send the passcode
+            debug("Sending passcode...");
             await performRpc(
                 socket,
                 new RemotePlayPasscodeResponsePacket(passCode),
                 RemotePlayResponseType.Login,
             );
         }
+
+        debug("Done!");
     }
 }
